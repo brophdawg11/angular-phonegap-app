af9ed2570802e0d29875a6f396b93fe83da6da03
Matt Brophy <matt@brophy.org>
2015-01-15 21:22:43 -0500

    e2e test update

--- test/e2e-spec.js
+++ test/e2e-spec.js
@@ -1,5 +1,5 @@
 describe('ng-phonegap-app test', function () {
-  it('should add a todo', function () {
+  it('should bootstrap the app', function () {
     browser.get('http://localhost:8000/test/');
     expect(element(by.id('test-div')).getText()).toEqual("This is the content");
   });
